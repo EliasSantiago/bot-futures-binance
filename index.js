6c166a7f647d4bfa9c35ab0bf4ca4e2133f7fbad
Elias Fonseca <dev.eliasfonseca@outlook.com>
2023-04-24 12:37:41 -0300

    fix

--- index.js
+++ index.js
@@ -25,17 +25,19 @@ app.use('/trandingview-btcusdt-buy', async (req, res, next) => {
 })
 
 app.use('/trandingview-btcusdt-sell', async (req, res, next) => {
-  console.log(req.originalUrl);
-  console.log(req.body);
-  const order = await api.newOrder(symbol, quantity, "SELL")
+  await api.setLeverage(symbol, leverage)
     .then(data => {
-      console.log(data);
-      res.json(data);
+      const order = api.newOrder(symbol, quantity, "SELL")
+        .then(data => {
+          res.json(data);
+        })
+        .catch(err => {
+          res.json(err);
+        })
     })
     .catch(err => {
-      console.error(err)
+      res.json(err);
     })
-  res.json(order);
 })
 
 app.use('/', async (req, res, next) => {
