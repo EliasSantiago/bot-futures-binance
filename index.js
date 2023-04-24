2bf984d472c34fd2d5d6323ab3476eadfd7907c3
Elias Fonseca <dev.eliasfonseca@outlook.com>
2023-04-24 09:50:37 -0300

    log

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ app.use('/trandingview-btcusdt-sell', async (req, res, next) => {
 })
 
 app.use('/', async (req, res, next) => {
-  res.json('Bot Futures Binance')
   console.log('Bot Futures Binance');
   console.log(process.env.PORT);
   console.log(process.env.API_KEY);
   console.log(process.env.SECRET_KEY);
+  res.json('Bot Futures Binance')
 })
 
 app.listen(process.env.PORT, () => {
