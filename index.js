7f4c4f0e7ce7294f451acbde34bb359c176a07e6
Elias Fonseca <dev.eliasfonseca@outlook.com>
2023-04-24 12:07:52 -0300

    fix

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use('/', async (req, res, next) => {
   // console.log(process.env.PORT);
   // console.log(process.env.API_KEY);
   // console.log(process.env.SECRET_KEY);
-  res.json(process.env.API_KEY);
+  //res.json(process.env.API_KEY);
 })
 
 app.listen(process.env.PORT, () => {
